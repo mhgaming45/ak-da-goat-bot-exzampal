7633826e0dea72a022d47de8e1ff91617bda80cb
mhgaming45 <marotimdpbn@gmail.com>
2026-07-31 13:33:59 +0530

    Refactor panel command and update embed message

--- commands/panel.js
+++ commands/panel.js
@@ -9,7 +9,7 @@ const {
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("panel")
-    .setDescription("Send Register Panel"),
+    .setDescription("Send the Register Panel"),
 
   async execute(interaction) {
 
@@ -17,40 +17,23 @@ module.exports = {
       .setColor("#5865F2")
       .setTitle("📝 Register Testing")
       .setDescription(
-`Click the button below to register for testing.
-
-After registering you can choose your gamemode.
-
-━━━━━━━━━━━━━━━━━━`
+        "Click **Register / Update** to register yourself for testing.\n\n" +
+        "After registration, you will be able to select your gamemode.\n\n" +
+        "━━━━━━━━━━━━━━━━━━━━"
       );
 
-    const register = new ActionRowBuilder().addComponents(
-      new ButtonBuilder()
-        .setCustomId("register")
-        .setLabel("Register / Update")
-        .setEmoji("📝")
-        .setStyle(ButtonStyle.Danger)
-    );
-
-    const row1 = new ActionRowBuilder().addComponents(
-      new ButtonBuilder().setCustomId("uhc").setLabel("UHC").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("pot").setLabel("Pot").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("mace").setLabel("Mace").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("nethop").setLabel("NetHop").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("smp").setLabel("SMP").setStyle(ButtonStyle.Secondary)
-    );
-
-    const row2 = new ActionRowBuilder().addComponents(
-      new ButtonBuilder().setCustomId("sword").setLabel("Sword").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("axe").setLabel("Axe").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("vanilla").setLabel("Vanilla").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("cart").setLabel("Cart").setStyle(ButtonStyle.Secondary)
-    );
+    const registerRow = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId("register")
+          .setLabel("Register / Update")
+          .setEmoji("📝")
+          .setStyle(ButtonStyle.Primary)
+      );
 
     await interaction.reply({
       embeds: [embed],
-      components: [register, row1, row2]
+      components: [registerRow]
     });
-
   }
 };
